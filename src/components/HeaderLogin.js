32f541795335dc3e81f38befe497fd38c73d88f3
Daniel Emery <danielremery@gmail.com>
2017-07-12 13:33:11 +0930

    Added HeaderLogin component

---
+++ src/components/HeaderLogin.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class HeaderLogin extends Component {
+    render() {
+        return (
+            <a className="nav-link" href="#">Login</a>
+        );
+    }
+}
+
+export default HeaderLogin;
