c78c8b7b942d181dbffa4024f2e601e25992c437
Daniel Emery <danielremery@gmail.com>
2017-07-12 13:30:43 +0930

    Decomposed header and footer into their own components

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+    render() {
+        return (
+            <div className="mastfoot">
+              <div className="inner">
+                <p>Landing page built based on cover template for <a href="https://v4-alpha.getbootstrap.com">Bootstrap</a>, by <a href="https://twitter.com/mdo">@mdo</a></p>
+              </div>
+            </div>
+        );
+    }
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import HeaderLogin from './HeaderLogin.js';
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="masthead clearfix">
+              <div className="inner">
+                <h3 className="masthead-brand">Natural Goodness</h3>
+                <nav className="nav nav-masthead">
+                  <a className="nav-link active" href="#">Home</a>
+                  <a className="nav-link" href="https://github.com/danielemery/natural-goodness" target="_blank">GitHub Project</a>
+                </nav>
+              </div>
+            </div>
+        );
+    }
+}
+
+export default Header;
