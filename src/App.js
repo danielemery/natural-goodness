c49f3a057cd2fdeec11da52a9ec475d2f1ed2fdc
Daniel Emery <danielremery@gmail.com>
2017-07-12 10:44:10 +0930

    Replicated master content

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import { Button } from 'reactstrap';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-          <br/>
-          <br/>
-          <Button color="danger">Here is a test button....</Button>
+      <div className="site-wrapper">
+        <div className="site-wrapper-inner">
+          <div className="cover-container">
+
+            <div className="masthead clearfix">
+              <div className="inner">
+                <h3 className="masthead-brand">Natural Goodness</h3>
+                <nav className="nav nav-masthead">
+                  <a className="nav-link active" href="#">Home</a>
+                  <a className="nav-link" href="https://github.com/danielemery/natural-goodness" target="_blank">GitHub Project</a>
+                </nav>
+              </div>
+            </div>
+
+            <div className="inner-cover">
+              <h1 className="cover-heading">Coming Soon</h1>
+              <p className="lead">A new way of managing the Natural Goodness Co-Op orders is currently in development. This will vastly simplify making your co-op order and increase visibility of filling orders etc.</p>
+            </div>
+
+            <div className="mastfoot">
+              <div className="inner">
+                <p>Landing page built based on cover template for <a href="https://v4-alpha.getbootstrap.com">Bootstrap</a>, by <a href="https://twitter.com/mdo">@mdo</a></p>
+              </div>
+            </div>
+
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
